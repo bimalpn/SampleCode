814281bec0682b3ee1926a62981ecdf00a580153
Nanda <265857@cognizant.com>
2017-03-21 19:52:24 -0500

    committed sample change

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+    	System.out.println("Hello Docker from the Console-Looks good");
+        return "Hello Docker World from the Console";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
